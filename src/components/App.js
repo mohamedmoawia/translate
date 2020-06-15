a4d1726d0cad6e465b1df29a3237afa8e434a896
mohamedmoawia <mo3wia94@gmail.com>
2020-06-15 19:02:11 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value="warning">
+
+                        <UserCreate />
+
+                    </ColorContext.Provider>
+                </LanguageStore>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/UserCreate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Field from './Field';
+import Button from './Button';
+
+const UserCreate = () =>{
+    return (
+        <div className='form-group'> 
+            <Field />
+            <Button />
+        </div>
+    )
+}
+
+export default UserCreate
