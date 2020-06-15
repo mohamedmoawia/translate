4e0c37631607b3731502121f0847eaea41c17377
mohamedmoawia <mo3wia94@gmail.com>
2020-06-15 19:03:40 +0200

    Updates

---
+++ precache-manifest.e69452e4b369af7e5f90f2e106346947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25acb2f31aaa2dde795fdf60314f01e",
+    "url": "/translate/index.html"
+  },
+  {
+    "revision": "9df89203c0ce33602e7c",
+    "url": "/translate/static/js/2.721c0c21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/translate/static/js/2.721c0c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d03f7a351881de3dbaf",
+    "url": "/translate/static/js/main.eba49451.chunk.js"
+  },
+  {
+    "revision": "16e53eb9ec47789f2e96",
+    "url": "/translate/static/js/runtime-main.6682f143.js"
+  }
+]);
